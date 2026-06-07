35b5e8f0c9da37876a93466b119582953999a4cd
Ganesh Mogal 07 <106980083+ganumogal@users.noreply.github.com>
2026-06-07 12:49:40 +0530

    Force HTTPS scheme and configure trusted proxies for Render

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -14,6 +14,7 @@ return Application::configure(basePath: dirname(__DIR__))
         $middleware->alias([
             'admin.auth' => \App\Http\Middleware\AdminAuth::class,
         ]);
+        $middleware->trustProxies(at: '*');
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
